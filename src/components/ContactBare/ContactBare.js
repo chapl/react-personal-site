2c51c37790b93526da99c72222bb9f310df095a1
Louis <chappelllouis@gmail.com>
2020-02-06 14:08:06 +0900

    changed window.open to window.location for improved compatability. Changed project list to keep on one line.

--- src/components/ContactBare/ContactBare.js
+++ src/components/ContactBare/ContactBare.js
@@ -7,12 +7,12 @@ import classes from './ContactBare.module.css'
 const ContactBare = () => {
     const sendMail = (event) => {
         event.preventDefault();
-        window.open(`mailto:${process.env.REACT_APP_EMAIL}`);
+        window.location.href = `mailto:${process.env.REACT_APP_EMAIL}`;
     }
 
     const github = (event) => {
         event.preventDefault();
-        window.open(process.env.REACT_APP_GITHUB);
+        window.location.href = process.env.REACT_APP_GITHUB;
     }
 
     return (

aab89b512293d3c0fc8f4835b3939a0288912a37
Louis <chappelllouis@gmail.com>
2020-02-06 12:29:38 +0900

    Background video wasn't autoplaying on iphone workaround noted in components/ui/background.js

--- src/components/UI/Background/Background.js
+++ src/components/UI/Background/Background.js
@@ -2,18 +2,29 @@ import React from 'react';
 import darkpark from "../../../assets/videos/darkpark.mp4";
 import classes from './Background.module.css';
 
+// React has a problem with muted for some reason, doesn't work on iphone and prevents autoplay.
+// See https://github.com/facebook/react/issues/6544
 
 const Video = () => (
-    <div className={classes.VidContainer}>
-        <video className={classes.Video} playsinline autoPlay muted loop src={darkpark} type="video/mp4">
-            Your browser does not support HTML5 video.
-        </video>
-        <div className={classes.Cover}></div>
+    // <div className={classes.VidContainer}>
+    //     <video className={classes.Video} playsinline autoPlay muted loop src={darkpark} type="video/mp4">
+    //         Your browser does not support HTML5 video.
+    //     </video>
+    // </div>
+    <div className={classes.VidContainer}
+        dangerouslySetInnerHTML={{ __html: `
+            <video
+            loop
+            muted
+            autoplay
+            playsinline
+            src="${darkpark}"
+            />,
+        ` }}>
     </div>
+
 
 );
 
 export default Video;
 
-// can i ref and set muted after?
-// ref={ref => ref.children[0]}
